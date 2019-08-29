65256716d7e335079db29aee64edd7bf209995a8
Abood <a.afifi5299@gmail.com>
2019-08-29 16:12:46 +1000

    Heroku Integration

--- web/server.js
+++ web/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const base = `${__dirname}/public`;
 
-app.use(express.static('public'));
+app.use(express.static(base));
 
 app.get('/login', (req, res) => {
     res.sendFile(`${base}/login.html`);
