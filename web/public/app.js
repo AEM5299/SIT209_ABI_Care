1113bb0145d416ab3bb8b238bad4ff88b6856b3b
brianfahad <40971064+brianfahad@users.noreply.github.com>
2019-09-02 15:09:34 +1000

    Basic Updates

--- web/public/app.js
+++ web/public/app.js
@@ -85,8 +85,7 @@ $('#login').on('click', () => {
             Swal.fire({
                 type: 'error',
                 title: 'Oops...',
-                text: 'Something went wrong!',
-                footer: '<a href>Why do I have this issue?</a>'
+                text: 'Password is wrong!'
             })
 
 
